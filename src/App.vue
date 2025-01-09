812871ce2218df1c24660caee14a25ff86663db3
Augustine Joseph <augustine@Augustines-MacBook-Pro.local>
2025-01-09 11:00:50 +0530

    Initial commit with counter

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div class="counter">
+    <h1>{{ count }}</h1>
+    <div class="buttons">
+      <button @click="increaseCounter()">+</button>
+      <button @click="decreaseCounter()">-</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    increaseCounter() {
+      this.count++
+    },
+    decreaseCounter() {
+      this.count--
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.counter {
+  font-size: 40px;
+}
+
+.buttons button {
+  font-size: 40px;
+  widows: 100px;
+  margin: 0 10px;
+  padding: 0 20px;
+}
+</style>
